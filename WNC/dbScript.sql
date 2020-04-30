73e3adbcdfbcbd136dcdf0ea1f6d917efcce7c13
maivanthanh <maivanthanh.6898@gmail.com>
2020-04-30 21:41:35 +0700

    Add project files.

---
+++ WNC/dbScript.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[tblUsers]
+(
+    [iID] INT PRIMARY KEY IDENTITY(1,1), 
+	[sUsername] NVARCHAR(MAX) NOT NULL ,
+    [sPassword] NVARCHAR(MAX) NOT NULL, 
+    [isAdmin] BIT NOT NULL, 
+    [sName] NVARCHAR(MAX) NOT NULL, 
+    [dBirth] DATE NULL, 
+    [sDesc] NVARCHAR(MAX) NULL, 
+    [sAddress] NVARCHAR(MAX) NULL, 
+)
+
+SET IDENTITY_INSERT [dbo].[tblUsers] ON
+INSERT INTO [dbo].[tblUsers] ([iID], [sUsername], [sPassword], [isAdmin], [sName], [dBirth], [sDesc], [sAddress]) VALUES (1, N'admin@gmail.com', N'admin', 1, N'Admin', NULL, NULL, NULL)
+INSERT INTO [dbo].[tblUsers] ([iID], [sUsername], [sPassword], [isAdmin], [sName], [dBirth], [sDesc], [sAddress]) VALUES (2, N'test@gmail.com', N'test', 0, N'Test', NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[tblUsers] OFF;
+
+Create proc DoLogin
+@username nvarchar(max),
+@password nvarchar(max)
+as begin
+select * from tblUsers where [sUsername]= @username and  [sPassword]=@password
+end
---
+++ WNC/loginForm.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WNC
+{
+    public partial class loginForm : System.Web.UI.Page
+    {
+        private string sCnStr = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(sCnStr))
+            {
+                conn.Open();
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = conn;
+                comm.CommandType = CommandType.StoredProcedure;
+                comm.CommandText = "DoLogin";
+                comm.Parameters.AddWithValue("@username", username.Text);
+                comm.Parameters.AddWithValue("@password", password.Text);
+                SqlDataReader reader = comm.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    Session["name"] = username.Text.ToString();
+                    Response.Write(@"<script language='javascript'>alert('Login Success');</script>");
+                    Response.Write(@"<script language='javascript'>console.log(" + Session["name"].ToString() + ");</script>");
+                    //Response.Redirect("index.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Sai tên đăng nhập hoặc mật khẩu')</script>");
+                }
+            }
+        }
+    }
+}
